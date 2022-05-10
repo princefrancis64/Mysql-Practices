ee9142355efe1ab6a64d6d07692433cd211511d2
Prince Francis <prince.francis64@gmail.com>
2022-05-10 19:26:18 +0530

    DDL practice

--- sql-basics/Basics.sql
+++ sql-basics/Basics.sql
@@ -57,6 +57,71 @@ update shipping_mode_dimen
 set football_team= 'Liverpool'
 where Vehicle_Company = 'Bike';
 
+
+create database market_star_schema;
+use market_star_schema;
+-- Creating the table  
+create table shipping_mode_dimen(
+Ship_Mode varchar(25),
+Vehicle_Company varchar(25),
+Toll_Required boolean
+);
+
+-- Making ship mode the primary key 
+alter table shipping_mode_dimen
+add constraint primary key(Ship_Mode);
+-- Inserting values into the table
+insert into shipping_mode_dimen(Ship_Mode, Vehicle_Company, Toll_Required)
+values
+('Delivery Truck','Ashok Leyland', false),
+('Regular Air','Air India', false);
+
+-- viewing the table as we have created 
+select * from shipping_mode_dimen;
+
+update shipping_mode_dimen
+set Toll_Required= true
+where Vehicle_Company= 'Air India';
+
+-- checking again the table to view the updated value 
+select * from shipping_mode_dimen;
+
+delete from shipping_mode_dimen
+where Vehicle_Company="Air India";
+
+select * from shipping_mode_dimen;
+
+alter table shipping_mode_dimen
+add Shipping_Amount int;
+
+-- updating the newly added column 
+update shipping_mode_dimen
+set Shipping_Amount = 5000
+where Vehicle_Company = "Ashok Leyland";
+
+select* from shipping_mode_dimen;
+
+
+-- changing the column name
+alter table shipping_mode_dimen
+change Shipping_Amount Shipping_Price int;
+
+select * from shipping_mode_dimen;
+
+ -- Deleting the newly added column 
+alter table shipping_mode_dimen
+drop column Shipping_Price;
+
+-- Deleting the whole table
+drop table shipping_mode_dimen;
+
+-- Deleting the database itself
+drop database market_star_schema;
+
+
+
+
+
 delete from shipping_mode_dimen
 where football_team= 'Liverpool';
 alter table shipping_mode_dimen
